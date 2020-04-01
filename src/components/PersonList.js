eb28fe18270a3289d242e72ef38647bb680023b3
bz699 <zeebha@gmail.com>
2020-04-01 11:18:49 +0200

    create new file PersonList.js

---
+++ src/components/PersonList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Contact from './Contact';
+
+function PersonIndex () {
+    const Person = [
+        {
+        pic : 'https://randomuser.me/api/portraits/men/16.jpg',
+        name : 'Daryl Brewer',
+        online : true,
+    },
+    {
+        pic : 'https://randomuser.me/api/portraits/men/4.jpg',
+        name : 'Ted Jenkins',
+        online : true,
+    },
+    {
+        pic : 'https://randomuser.me/api/portraits/women/60.jpg',
+        name : 'Leona Jones',
+        online : true,
+    },
+    {
+        pic : 'https://randomuser.me/api/portraits/men/58.jpg',
+        name : 'Nelson Mendoza',
+        online : false,
+    },
+    ]
+};
+
+export default PersonList;
