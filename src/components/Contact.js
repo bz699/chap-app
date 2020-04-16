661820c743e356c798c934c3eef45ca620539980
bz699 <zeebha@gmail.com>
2020-04-16 14:42:28 +0200

    add eventlistener on onwatch/offwatch indicator

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,44 @@ import React from 'react';
 import './Contact.css';
 
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img src={props.pic} alt="a portrait" className="avatar" />
-      <div>
-        <div className="name">{props.name}</div>
-        <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"} />
-          <div className={props.online ? "status-text-online" : "status-text-offline"}>On watch</div>
+class Contact extends React.Component {
+  constructor (props) {
+    super(props)
+      this.state = {
+        online: props.online,
+      };
+  }
+
+/*<div className={props.online ? "status-online" : "status-offline"} />
+  <div className={props.online ? "status-text-online" : "status-text-offline"}>On watch</div> */
+
+
+  render (){
+    return (
+      <div className="Contact">
+        <img src={this.props.pic} alt="a portrait" className="avatar" />
+        <div>
+          <div className="name">{this.props.name}</div>
+
+          <div className="status">
+          
+            <div className={this.state.online ? "status-online" : "status-offline"} />
+          
+            <p className="status-text"
+              onClick= { event => {
+                const newStatus =! this.state.online;
+                this.setState({online : newStatus});
+              }}>
+              {this.state.online ? "onwatch" : "offwatch"}
+            </p>
+          
+          </div>
+
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-  export default Contact;
+
+export default Contact;
--- src/components/SendButton.js
+++ src/components/SendButton.js
@@ -5,10 +5,6 @@ import './SendButton.css';
 class SendButton extends React.Component {
 
 eventListener = (event) => {
-    const id = event.target.id;
-    const type = event.target.type;
-    const className = event.target.className;
-    const text = event.target.innerText;
     alert(`We will watch`);
   };
 
